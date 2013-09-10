1e723f430952b5120f4b1e347a3da0362abc1cea
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2013-09-10 21:40:10 +0900

    delete unused code

--- app/controllers/user_bokes.rb
+++ app/controllers/user_bokes.rb
@@ -1,24 +1,4 @@
-BoketeApiServer::App.controllers :user_bokes do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
+BoketeApiServer::App.controllers :user_bokes do  
   get :index do    
     q = params[:q]
     if q != nil
